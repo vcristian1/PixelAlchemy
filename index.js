3c8aef0186585c87ba7483753ee5169b3c4d0e79
Vargas <cristian.a.vargas@accenture.com>
2024-01-23 13:09:24 -0600

    small change to PORT

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const path = require('path')
 const express = require('express');
 const dotenv =  require('dotenv').config();
 const { generateImage } = require('./controllers/openaiController')
-const port = process.env.PORT || 5000;
-
+const port = process.env.PORT
 const app = express();
 
 //Middleware app.use
