8ae8c9bebbb0de3bfc6b68ba967a5ed1af9f8121
Vargas <cristian.a.vargas@accenture.com>
2024-01-23 13:23:36 -0600

    removed line of code that was unneccessary

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const path = require('path')
 const express = require('express');
 const dotenv =  require('dotenv').config();
-const { generateImage } = require('./controllers/openaiController')
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
+
 const app = express();
 
-//Middleware app.use
+//Middleware (app.use(...))
 
 ///Enable body parser, so we can parse the request.
 app.use(express.json());
