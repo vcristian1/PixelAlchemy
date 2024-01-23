8e248eab97c602575aaaf87d3367f0c2fd2c1ac0
Vargas <cristian.a.vargas@accenture.com>
2024-01-23 13:10:55 -0600

    reverted port change

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const path = require('path')
 const express = require('express');
 const dotenv =  require('dotenv').config();
 const { generateImage } = require('./controllers/openaiController')
-const port = process.env.PORT
+const port = process.env.PORT || 5000
 const app = express();
 
 //Middleware app.use
